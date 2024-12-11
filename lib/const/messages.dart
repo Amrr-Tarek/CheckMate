525b8b5522bbb58499a77fa7350244b4568ef5c4
mohamed-yehia311 <mmoa311311@gmail.com>
2024-12-11 23:23:42 +0200

    create messages

---
+++ lib/const/messages.dart
@@ -0,0 +1,4 @@
+class Messages {
+  static const String finishTasks = "Checkmate! No tasks left to complete. ";
+  static const String motivationToAddTasks = "Your next move starts here\nadd a task and checkmate your goals!";
+}
