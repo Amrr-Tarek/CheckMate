703971c5f0f60a38bf5f2a934e99d9ad835e4889
mohamed-yehia311 <mmoa311311@gmail.com>
2024-12-12 19:50:54 +0200

    update messages

--- lib/const/messages.dart
+++ lib/const/messages.dart
@@ -1,4 +1,12 @@
 class Messages {
-  static const String finishTasks = "Checkmate! No tasks left to complete. ";
-  static const String motivationToAddTasks = "Your next move starts here\nadd a task and checkmate your goals!";
+  static const String finishActivities = "Checkmate! No activities left to complete. ";
+  static const String motivationToAddActivities = "Your next move starts here\nadd an activity and checkmate your goals!";
+  static const String addActivity = "Add New activity";
+  static const String enterActivityDescription = "Enter activity description";
+  static const String cancelButtonText = "Cancel";
+  static const String addButtonText = "Add activity";
+  static const String enterActivityInterval = "Enter time interval";
+  static const String activityInputError = "please enter both description and interval correctly";
+  static const String saveButtonText = "Save";
+  static const String editActivity = "Edit";
 }
