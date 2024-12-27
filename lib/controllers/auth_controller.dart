84b6110175ab682e9f925f283cc657a3fd194368
Amr Tarek <148470882+Amrr-Tarek@users.noreply.github.com>
2024-12-27 14:03:35 +0200

    sign_in_with_google

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,10 +1,11 @@
-/// This file contains the AuthController class, which is responsible for handling user authentication.
-/// Register, Login, and Logout functions are defined in this class.
-
 import 'package:checkmate/controllers/firestore_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/// This file contains the AuthController class, which is responsible for handling user authentication.
+/// Register, Login, and Logout functions are defined in this class.
 
 class AuthController {
   // final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -91,6 +92,63 @@ class AuthController {
       );
     } catch (e) {
       print("Error signing up: $e");
+    }
+  }
+
+  Future<void> signInWithGoogle({required BuildContext context}) async {
+    try {
+      // Sign in with Google
+      final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+      if (gUser == null) return;
+
+      final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+
+      // Create new credential for user
+      final credential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
+      );
+
+      // Sign in to Firebase
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      // Extract user Info
+      final User? user = userCredential.user;
+      if (user != null) {
+        final String name = user.displayName ?? "Unknown User";
+        final String email = user.email ?? "Blank Email";
+
+        FirestoreDataSource().createUser(name, email);
+      }
+
+      Navigator.pushReplacementNamed(context, "/home");
+    } catch (e) {
+      print("Exception $e");
+    }
+  }
+
+  Future<UserCredential?> signInWithGithub(
+      {required BuildContext context}) async {
+    try {
+      // Sign in with Github
+      GithubAuthProvider githubAuthProvider = GithubAuthProvider();
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithProvider(githubAuthProvider);
+
+      // Extract user Info
+      final User? user = userCredential.user;
+      if (user != null) {
+        final String name = user.displayName ?? "Unknown User";
+        final String email = user.email ?? "Blank Email";
+
+        FirestoreDataSource().createUser(name, email);
+      }
+      Navigator.pushReplacementNamed(context, "/home");
+      return userCredential;
+    } catch (e) {
+      print("Exception: $e");
+      return null;
     }
   }
 
